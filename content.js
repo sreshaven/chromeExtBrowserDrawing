120e1e293d8ee4663e4d47ee022713b79d7c5d5f
sreshaven <sreshaven@gmail.com>
2021-07-28 20:14:59 -0500

    background

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
 console.log("Chrome Extension Ready To Go!");
+ // c.style('z-index', '999')
 
-let paragraphs = document.getElementsByTagName('p');
+ let paragraphs = document.getElementsByTagName('p');
+ for (elem of paragraphs){
+   elem.style['background-color'] = '#FF00FF';
+ }
