d927f36a13f2d14e43b62a61d906f63553dfa932
sreshaven <sreshaven@gmail.com>
2021-07-29 19:14:56 -0500

    chrome extension color pen

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-console.log("Background Running");
+// SAVE AND REFRESH EXTENSION!!!!
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab){
+  let msg = {
+    txt: "run"
+  }
+  chrome.tabs.sendMessage(tab.id, msg);
+}
--- content.js
+++ content.js
@@ -1,7 +1,54 @@
-console.log("Chrome Extension Ready To Go!");
- // c.style('z-index', '999')
 
- let paragraphs = document.getElementsByTagName('p');
- for (elem of paragraphs){
-   elem.style['background-color'] = '#FF00FF';
+let started = false;
+// initialize brush color
+let brushHue;
+
+ chrome.runtime.onMessage.addListener(gotMessage);
+
+ function gotMessage(message, sender, sendResponse){
+   if (message.txt === "run"){
+     started = true;
+   }
+ }
+
+ function setup(){
+   // user cannot select text when drawing
+   if(started){
+     document.body.style['userSelect'] = 'none';
+   // set canvas to page with clear canvas
+     let h = document.body.clientHeight;
+     let c = createCanvas(windowWidth, h);
+     c.position(0, 0);
+     clear();
+   // can click on items
+     c.style('pointer-events','none');
+   // can draw on anything
+     c.style('z-index', '999')
+   // setting pen style
+     colorMode(HSB, 360, 100, 100);
+     brushHue = 0;
+     strokeWeight(6);
+   }
+ }
+
+ function draw(){
+   if(started){
+     chooseColors();
+     if(mouseIsPressed){
+       line(pmouseX, pmouseY, mouseX, mouseY);
+     }
+   }
+ }
+ // changing colors pen
+ function chooseColors() {
+   if (brushHue > 360) {
+       brushHue = 0;
+     }
+   brushHue += 1;
+   stroke(brushHue, 50, 100);
+   fill(brushHue, 50, 100);
+ }
+ // clear canvas is any key is pressed
+ function keyPressed(){
+   clear();
  }
